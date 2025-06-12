7a50902730aa68b245969d3503d5cba26265d071
sebascg17 <sebas-clavi@hotmail.com>
2025-06-12 13:22:39 -0500

    Merge branch 'Sebas-Dev'

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private Vector2 direction;
+    private float speed;
+    private float lifetime = 3f;
+    private GameObject gameManager;
+    [SerializeField] private int daño = 1;
+    [SerializeField] private int puntos = 10;
+
+    private Rigidbody2D rb;
+
+    // NUEVO: Límites de pantalla
+    private float minX, maxX, minY, maxY;
+
+    void Awake ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start ()
+    {
+        Destroy(gameObject, lifetime);
+        gameManager = GameObject.Find("Game Manager");
+    }
+
+    public void SetDirection ( Vector2 dir, float spd )
+    {
+        direction = dir.normalized;
+        speed = spd;
+
+        rb.gravityScale = 0f;
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        rb.velocity = direction * speed;
+    }
+
+    // NUEVO: Recibir los límites desde el PlayerController
+    public void SetScreenLimits ( float minX, float maxX, float minY, float maxY )
+    {
+        this.minX = minX;
+        this.maxX = maxX;
+        this.minY = minY;
+        this.maxY = maxY;
+    }
+
+    void Update ()
+    {
+        // Chequea cada frame si está fuera de la pantalla
+        if (transform.position.x < minX || transform.position.x > maxX ||
+            transform.position.y < minY || transform.position.y > maxY)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D ( Collider2D collision )
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            collision.GetComponent<Enemy>().RecibirDaño(daño);
+            gameManager.GetComponent<GameManager>().SumarPuntaje(puntos);
+            Debug.Log("Enemigo golpeado, puntos sumados: " + puntos);
+            Debug.Log("Enemigo golpeado, daño infligido: " + daño);
+        }
+        else if (collision.CompareTag("Player"))
+        {
+            GameManager.Instance.RestarVida(daño);
+            Destroy(gameObject);
+        }
+
+        else if (collision.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,191 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    [Header("Nivel")]
+    [SerializeField] private int nivel = 1;
+
+    [Header("Movimiento")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+
+    [Header("Salto Mejorado")]
+    [SerializeField] private float fallMultiplier = 2.5f;
+    [SerializeField] private float lowJumpMultiplier = 2f;
+
+    [Header("Disparo")]
+    [SerializeField] public GameObject[] bulletPrefab;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private float fireCooldown;
+    [SerializeField] private float offsetBullet = 0.5f;
+
+    [Header("Audio")]
+    [SerializeField] private AudioClip audioJump;
+    [SerializeField] private AudioClip audioDisparo;
+
+    [Header("Limite de Pantalla")]
+    [SerializeField] private float minX, maxX, minY, maxY;
+
+    [Header("Powerup")]
+    [SerializeField] private GameObject powerup;
+
+    private AudioSource audioSource;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private float horizontalInput;
+    private bool facingRight = true;
+    private float fireCooldownTimer = 0f;
+
+    void Start ()
+    {
+        powerup = GameObject.Find("Powerup");
+        audioSource = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+    }
+
+    void Update ()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        Mover();
+        Saltar();
+        MejorarSalto();
+        Disparar();
+        fireCooldownTimer -= Time.deltaTime;
+    }
+
+    void LateUpdate ()
+    {
+        LimitarPosicion();
+    }
+
+    void LimitarPosicion ()
+    {
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(transform.position.x, minX, maxX);
+        clampedPosition.y = Mathf.Clamp(transform.position.y, minY, maxY);
+        transform.position = clampedPosition;
+    }
+
+    void Mover ()
+    {
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+
+        if (horizontalInput > 0)
+            facingRight = true;
+        else if (horizontalInput < 0)
+            facingRight = false;
+    }
+
+    void Saltar ()
+    {
+        if(nivel == 1) {
+            if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+            {
+                Debug.Log("Salto detectado");
+                audioSource.PlayOneShot(audioJump);
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                Debug.Log("Salto realizado");
+            }
+        }
+        if (nivel == 2) {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                audioSource.PlayOneShot(audioJump);
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                Debug.Log("Salto realizado");
+            }
+        }
+    }
+
+    void MejorarSalto ()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void Disparar ()
+    {
+        Vector3 offset = facingRight ? Vector3.right * offsetBullet : Vector3.left * offsetBullet;
+        if (nivel == 1) {
+            if (Input.GetKeyDown(KeyCode.Space) && fireCooldownTimer <= 0f)
+            {
+                audioSource.PlayOneShot(audioDisparo);
+                GameObject bullet = Instantiate(bulletPrefab[0], firePoint.position + offset, Quaternion.identity);
+                BulletScript bulletScript = bullet.GetComponent<BulletScript>();
+                bulletScript.SetDirection(facingRight ? Vector2.right : Vector2.left, bulletSpeed);
+                bulletScript.SetScreenLimits(minX, maxX, minY, maxY); // <-- NUEVO
+            }
+        }
+
+        if (nivel == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && fireCooldownTimer <= 0f)
+            {
+                audioSource.PlayOneShot(audioDisparo);
+                GameObject bullet = Instantiate(bulletPrefab[1], firePoint.position + offset, Quaternion.identity);
+                BulletScript bulletScript = bullet.GetComponent<BulletScript>();
+                bulletScript.SetDirection(facingRight ? Vector2.right : Vector2.left, bulletSpeed);
+                bulletScript.SetScreenLimits(minX, maxX, minY, maxY); // <-- NUEVO
+            }
+        }
+    }
+
+    public void RecibirDaño ( int daño )
+    {
+        GameManager.Instance.RestarVida(daño);
+        nivel--;
+    }
+
+
+    void Morir ()
+    {
+        Debug.Log("Enemigo muerto");
+        Destroy(gameObject);
+    }
+
+    // -------- AQUÍ VA LO NUEVO ---------
+
+    private void OnCollisionEnter2D ( Collision2D collision )
+    {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Obstacle"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D ( Collision2D collision )
+    {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Obstacle"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnTriggerEnter2D ( Collider2D other )
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            nivel++;
+            Debug.Log("PowerUp recogido. Nivel actual: " + nivel);
+        }
+        else if (other.gameObject.CompareTag("ObstacleDañino"))
+        {
+            GameManager.Instance.RestarVida(1);  // o la cantidad de daño que quieras
+            Debug.Log("Jugador golpeado por obstáculo dañino");
+        }
+    }
+
+}
